ce2af0b692c0f5d43d0183bffc310eae1911e1cd
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-05-29 19:46:48 +0530

    Styles navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,10 +6,10 @@ class Navbar extends React.Component{
          
         return(
 
-            <div className="">
-                <div>
-                    <img src="https://image.flaticon.com/icons/png/128/879/879815.png" alt="Navbar"/>
-                    <span>3</span>
+            <div style={styles.nav}>
+                <div style={styles.cartIconContainer}>
+                    <img style={styles.cartIcon}src="https://image.flaticon.com/icons/png/128/879/879815.png" alt="Navbar"/>
+                    <span style={styles.cartCount}>3</span>
                 </div>
                 
             </div>
@@ -18,12 +18,29 @@ class Navbar extends React.Component{
 }
 
 const styles = {
-    image:{
-        height: 110,
-        width:110,
-        borderRadius:4,
-        background:'blue'
+    cartIcon:{
+        height: 40,
+         marginRight:30
+    },
+    nav:{
+        height:70,
+        background:'aqua',
+        display:'flex',
+        justifyContent:'flex-end',
+        alignItems:'center'
+    },
+    cartIconContainer:{
+        position:'relative'
+    },
+    cartCount:{
+        background:'yellow',
+        borderRadius: '50%',
+        padding:'4px 8px',
+        position:'absolute',
+        right: 15,
+        top: -9
     }
+
 }
 
 export default Navbar;
