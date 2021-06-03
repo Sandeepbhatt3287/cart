91fb6666112916d7608ddde0a1020a7d49ae4f75
Sandeep Bhatt <sandeepb3287@gmail.com>
2021-06-03 19:21:40 +0530

    adding firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import  firebase from 'firebase';
+import 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7izIFFaVW8U_5qnKL98ISeY6ppIe77ho",
+  authDomain: "cart-e3dc4.firebaseapp.com",
+  projectId: "cart-e3dc4",
+  storageBucket: "cart-e3dc4.appspot.com",
+  messagingSenderId: "293888126941",
+  appId: "1:293888126941:web:4b517d80de87e80f29d034"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 ReactDOM.render(
   <React.StrictMode>
